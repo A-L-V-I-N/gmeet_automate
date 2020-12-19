d4c60527584f108e7dea8791c149243d95ee191a
Alvin Antony <72436408+A-L-V-I-N@users.noreply.github.com>
2020-12-19 14:39:58 +0530

    Update and rename gmeet_automate.py to gmeet_automate_win.py

---
+++ gmeet_automate_win.py
@@ -0,0 +1,26 @@
+
+from datetime import datetime
+from pynput.keyboard import Key, Controller
+import time, os
+import webbrowser
+keyboard = Controller()
+
+link = "ttd-hnxt-iyi"         #set the link here
+meet_time = ("12:26:00")   #set the time here
+
+def presstabkey(num, tym, keys):
+	
+	time.sleep(tym)
+	while num > 0 :
+		keyboard.press(keys)
+		keyboard.release(keys)
+		num = num-1
+	keyboard.press(Key.enter)
+	keyboard.release(Key.enter)
+
+while True:
+	now = datetime.now()
+	if now.strftime("%H:%M:%S")  == meet_time:
+	    webbrowser.open("http://meet.google.com/"+link)
+	    presstabkey(8, 8, Key.tab)
+	    break
